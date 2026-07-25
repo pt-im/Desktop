1800420b7bbd532bdc61460b16bcfaca58c966d8
Plus <channellast061@gmail.com>
2026-07-25 10:52:51 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'pokemon_7025.ico'),
+    icon: path.join(__dirname, 'female_woman_girl_person_user_client_1470.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
